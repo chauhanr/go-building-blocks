ca514ee80c3de0751cadf663875d689d65e1b315
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2019-12-11 06:57:07 +0000

    url shortner

--- url-shortner/handlers/handler.go
+++ url-shortner/handlers/handler.go
@@ -1,12 +1,56 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
 
-func MapHandler(pathTourls map[string]string, fallback http.Handler) http.HandlerFunc {
+	"gopkg.in/yaml.v2"
+)
 
-	mapHandler := func(w http.ResponseWriter, r *http.Request) {
+func YamlHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYaml(yaml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap, _ := buildMap(parsedYaml.UrlMappings)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYaml(yamlb []byte) (UrlMap, error) {
+	urlMapping := UrlMap{}
+	err := yaml.Unmarshal(yamlb, &urlMapping)
+	if err != nil {
+		return UrlMap{}, err
+	}
+	return urlMapping, nil
+}
+
+var internalMap = map[string]string{
+	"/twitter":  "wwww.twitter",
+	"/facebook": "www.facebook.com",
+	"/eco":      "www.economist.com",
+}
+
+func buildMap(parsedYaml map[string]string) (map[string]string, error) {
+	for k, v := range parsedYaml {
+		internalMap[k] = v
+	}
+	return internalMap, nil
+}
+
+func MapHandler(pathMap map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		fmt.Printf("Path: %s\n", path)
+		if dest, ok := pathMap[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
 
 	}
+}
 
-	return mapHandler
+type UrlMap struct {
+	UrlMappings map[string]string `"yaml:UrlMappings"`
 }
--- url-shortner/main.go
+++ url-shortner/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main(){
-   
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/chauhanr/go-building-blocks/url-shortner/handlers"
+)
+
+func main() {
+	mux := defaultMux()
+	yaml := `UrlMappings:
+        "/gogl": "www.google.com"
+        "/yah": "www.yahoo.com"
+        "/hcl": "www.hcl.com"`
+
+	yHandler, err := handlers.YamlHandler([]byte(yaml), mux)
+	if err != nil {
+		panic(err)
+	}
+	http.ListenAndServe(":9090", yHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+
 }
